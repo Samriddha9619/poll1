abf8f4b83e2b7a265735c758c88b51bad38707ed
Samriddha9619 <sumitkumartripathi0@gmail.com>
2025-02-07 19:07:30 +0530

    commit 4

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import question
 
-admin.site.register(question)
+from .models import Question
+
+admin.site.register(Question)
--- polls/models.py
+++ polls/models.py
@@ -1,17 +1,25 @@
-import datetime 
+import datetime
+
 from django.db import models
 from django.utils import timezone
 
-class question(models.Model):
-    def was_made_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
     def __str__(self):
         return self.question_text
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("Date published")
-class choice(models.Model):
-    def __str__(self):
-        return self.choice_text
-    question = models.ForeignKey(question, on_delete=models.CASCADE)
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
